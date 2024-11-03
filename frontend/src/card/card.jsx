2bdbffa06f298ffeec1eb6874e8ac63f5ec1c613
Nick <nickhosman@gmail.com>
2024-11-02 21:15:04 -0700

    basic card style

--- frontend/src/card/card.jsx
+++ frontend/src/card/card.jsx
@@ -1,9 +1,9 @@
 export default function Card() {
     return (
-        <div className="rounded-lg border-black border-2 h-96 w-64 flex flex-col items-center ">
+        <div className="rounded-lg border-black border-2 h-96 w-64 flex flex-col items-center bg-green-300">
             <div className="mt-4 mb-2 w-2/3">Card Name</div>
-            <div className="box-border border-2 border-black w-4/5 h-2/5"></div>
-            <div>
+            <div className="bg-white box-border border-2 border-black w-4/5 h-2/5"></div>
+            <div className="bg-green-200">
                 <p className="p-2 text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tristique enim eget condimentum gravida. Vivamus ut enim porttitor, vulputate augue eu, consequat diam. In faucibus ullamcorper nunc et ornare. Mauris hendrerit libero ac libero semper vulputate. Vivamus tempus, dui vel eleifend venenatis, ipsum est fermentum nibh, vitae finibus tortor erat laoreet purus.</p>
             </div>
         </div>
